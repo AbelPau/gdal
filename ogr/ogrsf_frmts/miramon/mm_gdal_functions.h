0f2a39b52f5adee20eb14238e87cf7a45553f216
AbelPau <a.pau@creaf.uab.cat>
2024-04-12 15:55:37 +0200

    n_decimals not used anymore

--- ogr/ogrsf_frmts/miramon/mm_gdal_functions.h
+++ ogr/ogrsf_frmts/miramon/mm_gdal_functions.h
@@ -118,10 +118,8 @@ int MM_DuplicateFieldDBXP(struct MM_FIELD *camp_final,
                           const struct MM_FIELD *camp_inicial);
 int MM_WriteNRecordsMMBD_XPFile(struct MMAdmDatabase *MMAdmDB);
 
-size_t MM_DefineFirstPolygonFieldsDB_XP(struct MM_DATA_BASE_XP *bd_xp,
-                                        MM_BYTE n_decimals);
-size_t MM_DefineFirstArcFieldsDB_XP(struct MM_DATA_BASE_XP *bd_xp,
-                                    MM_BYTE n_decimals);
+size_t MM_DefineFirstPolygonFieldsDB_XP(struct MM_DATA_BASE_XP *bd_xp);
+size_t MM_DefineFirstArcFieldsDB_XP(struct MM_DATA_BASE_XP *bd_xp);
 size_t MM_DefineFirstNodeFieldsDB_XP(struct MM_DATA_BASE_XP *bd_xp);
 size_t MM_DefineFirstPointFieldsDB_XP(struct MM_DATA_BASE_XP *bd_xp);
 int MM_ModifyFieldNameAndDescriptorIfPresentBD_XP(
