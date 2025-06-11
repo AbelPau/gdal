c75e1f405852c84a419422af21a20e09d890105c
AbelPau <a.pau@creaf.uab.cat>
2025-06-11 15:01:33 +0200

    test

--- autotest/gcore/miramon_read.py
+++ autotest/gcore/miramon_read.py
@@ -18,8 +18,8 @@ import pytest
 # from osgeo import gdal
 
 init_list = [
-    # ("byte_2x3_6_categs_RLE.img", 15),
-    ("byte_2x3_6_categs.img", 15),
+    # ("data/miramon/byte_2x3_6_categs_RLE.img", 15),
+    ("data/miramon/byte_2x3_6_categs.img", 15),
 ]
 
 
@@ -29,6 +29,6 @@ init_list = [
     ids=[tup[0].split(".")[0] for tup in init_list],
 )
 @pytest.mark.require_driver("MiraMonRaster")
-def test_hfa_open(filename, checksum):
+def test_miramon_open(filename, checksum):
     ut = gdaltest.GDALTest("MiraMonRaster", filename, 1, checksum)
     ut.testOpen()
