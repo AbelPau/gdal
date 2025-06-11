538c83a5ba28e9b18249213a0e399922dd906c23
AbelPau <a.pau@creaf.uab.cat>
2025-06-11 17:22:09 +0200

    first test ok

--- autotest/gcore/miramon_read.py
+++ autotest/gcore/miramon_read.py
@@ -12,23 +12,27 @@
 ###############################################################################
 
 
-import gdaltest
+# import gdaltest
 import pytest
 
-# from osgeo import gdal
+from osgeo import gdal
 
 init_list = [
-    # ("data/miramon/byte_2x3_6_categs_RLE.img", 15),
-    ("data/miramon/byte_2x3_6_categs.img", 15),
+    # ("data/miramon/byte_2x3_6_categs_RLE.img", 1, 15),
+    ("data/miramon/byte_2x3_6_categs.img", 1, 15),
 ]
 
 
 @pytest.mark.parametrize(
-    "filename,checksum",
+    "filename,band_idx,checksum",
     init_list,
     ids=[tup[0].split(".")[0] for tup in init_list],
 )
 @pytest.mark.require_driver("MiraMonRaster")
-def test_miramon_open(filename, checksum):
-    ut = gdaltest.GDALTest("MiraMonRaster", filename, 1, checksum)
-    ut.testOpen()
+def test_miramon_open(filename, band_idx, checksum):
+    ds = gdal.Open(filename)
+    assert ds is not None, "No s'ha pogut obrir el fitxer"
+    band = ds.GetRasterBand(band_idx)
+    assert band is not None, f"Error opening band {band_idx}"
+    rchecksum = band.Checksum()
+    assert rchecksum == checksum, f"Unexpected checksum: {checksum}"
