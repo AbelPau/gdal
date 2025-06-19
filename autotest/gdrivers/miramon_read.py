3559537842559c6234fee77104c67df6974a99de
AbelPau <a.pau@creaf.uab.cat>
2025-06-19 17:01:05 +0200

    bit tests

--- autotest/gdrivers/miramon_read.py
+++ autotest/gdrivers/miramon_read.py
@@ -25,7 +25,7 @@ init_list = [
     ("data/miramon/normal/real_2x3_6_categs.img", 1),
     ("data/miramon/normal/double_2x3_6_categs.img", 1),
     ("data/miramon/normal/byte_2x3_6_categs_RLE.img", 1),
-    # ("data/miramon/normal/byte_2x3_6_categs_RLE_no_ind.img", 1),
+    ("data/miramon/normal/byte_2x3_6_categs_RLE_no_ind.img", 1),
     ("data/miramon/normal/integer_2x3_6_categs_RLE.img", 1),
     ("data/miramon/normal/uinteger_2x3_6_categs_RLE.img", 1),
     ("data/miramon/normal/long_2x3_6_categs_RLE.img", 1),
@@ -60,3 +60,23 @@ def test_miramon_test_012345_raster(filename, band_idx):
     assert array[1][1] == 3, f"Unexpected pixel value: {1},{1},{3}"
     assert array[2][0] == 4, f"Unexpected pixel value: {2},{0},{4}"
     assert array[2][1] == 5, f"Unexpected pixel value: {2},{1},{5}"
+
+
+@pytest.mark.require_driver("MiraMonRaster")
+def test_miramon_test_bit_raster():
+    ds = gdal.Open("data/miramon/normal/chess_bit.img")
+    assert ds is not None, "Could not open the file"
+    band = ds.GetRasterBand(1)
+    assert band is not None, f"Error opening band {1}"
+
+    # Read the entire raster band as a NumPy array
+    array = band.ReadAsArray()
+    assert array is not None, "Could not read band as array"
+
+    # Loop through all pixels in the array
+
+    for i in range(8):
+        for j in range(8):
+            expected = (i + j) % 2
+            actual = array[i][j]
+            assert actual == expected, f"Unexpected pixel value: {i},{j},{actual}"
